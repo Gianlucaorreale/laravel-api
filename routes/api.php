e3ad36ee224e2d17389f8a8d3cd38cd4bf72f6f7
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-10-04 17:06:18 +0200

    modified api/PostController@index

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/posts','Api\PostController@index');
+Route::get('/posts', 'Api\PostController@index');
