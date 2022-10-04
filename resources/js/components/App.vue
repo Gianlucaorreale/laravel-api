6aa380fa38da2e2dd8abace8e3613437bf3f4cdf
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-10-04 17:50:01 +0200

    added AppHeader component in App.vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="content">
+       <AppHeader :title="title"/> 
+    </div>
+</template>
+
+<script>
+    import AppHeader from './AppHeader.vue';
+    export default {
+    name: "App",
+    data() {
+        return {
+            title: "Boolpress"
+        };
+    },
+    components: { AppHeader }
+};
+</script>
